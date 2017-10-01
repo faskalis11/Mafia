a3400dbfdf242cc32bd1da1468e3bc9991b3d72b
Ignas Jankauskas <ignasjankauskas9@gmail.com>
2017-10-01 13:04:51 +0300

    game roles test min max players

--- game_spec.rb
+++ game_spec.rb
@@ -7,10 +7,18 @@ describe 'My behaviour' do
 
   it 'should have roles for a game' do
     roles = ["Citizen", "Mafia"]
-    newGame = Game.new(roles)
+    newGame = Game.new(roles, nil, nil)
     expect(newGame.roles).not_to be_empty
   end
 
+  it 'should have minimum and maximum players limit' do
+    roles = ["Citizen", "Mafia"]
+    min, max = 5, 10
+    newGame = Game.new(roles, min, max)
+    expect(newGame.min).not_to be_empty
+    expect(newGame.max).not_to be_empty
+    expect(newGame.max).to >= newGame.min
+  end
 
 
 end
