9131b8a4d0b3792b927846f96f6a5ad71340b5bb
Ignas Jankauskas <ignasjankauskas9@gmail.com>
2017-10-01 13:42:09 +0300

    game join player created

--- game.rb
+++ game.rb
@@ -3,6 +3,7 @@ class Game
     @roles = roles
     @min = min
     @max = max
+    @players = []
   end
 
   def roles
@@ -15,5 +16,13 @@ class Game
 
   def min
     @min
+  end
+
+  def players
+    @players
+  end
+
+  def join(player)
+    @players.push(player)
   end
 end
