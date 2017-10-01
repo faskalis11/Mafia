e2ddafb5abba61bb127e587148ef2bd7b369b8bf
Ignas Jankauskas <ignasjankauskas9@gmail.com>
2017-10-01 13:17:30 +0300

    game join player

--- game_spec.rb
+++ game_spec.rb
@@ -13,12 +13,22 @@ describe 'My behaviour' do
 
   it 'should have minimum and maximum players limit. Maximum must not be lower than minimum ' do
     roles = ["Citizen", "Mafia"]
-    min, max = 5, 10
+    min, max = 4, 10
     new_game = Game.new(roles, min, max)
     expect(new_game.min).to be > 0
     expect(new_game.max).to be > 0
 
     expect(new_game.max).to be >= new_game.min
+  end
+
+  it 'should accept players to the game' do
+    roles = ["Citizen", "Mafia"]
+    min, max = 4, 10
+    new_game = Game.new(roles, min, max)
+    player1 = Player.new
+    new_game.join(player1)
+    expect(new_game.players).not_to be_empty
+
   end
 
 
