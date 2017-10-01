0b43d2ec6b9742165a496ea540148092f3444447
Ignas Jankauskas <ignasjankauskas9@gmail.com>
2017-10-01 12:56:06 +0300

    game roles test

---
+++ game_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require_relative 'player'
+#require_relative 'game'
+
+
+describe 'My behaviour' do
+
+  it 'should have roles for a game' do
+    roles = ["Citizen", "Mafia"]
+    newGame = Game.new(roles)
+    expect(newGame.roles).not_to be_empty
+  end
+
+
+end
